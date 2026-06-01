9702b5d835db93eaf4d45ad3395a087190e65b77
anupam-devcodes <anupamchoubey22@gmail.com>
2026-06-01 18:56:36 +0530

    Connect server to MongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,17 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDatabase from "./config/database.config.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`FinSentry server is running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  await connectDatabase();
+
+  app.listen(PORT, () => {
+    console.log(`FinSentry server is running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
