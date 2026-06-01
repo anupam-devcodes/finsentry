3910aecff56e2dc80cb66efb0de2252bf471e3ac
anupam-devcodes <anupamchoubey22@gmail.com>
2026-06-01 05:55:18 +0530

    Separate Express app configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,9 @@
-import express from "express";
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config();
 
-const app = express();
-
 const PORT = process.env.PORT || 5000;
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "FinSentry API is running successfully.",
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`FinSentry server is running on http://localhost:${PORT}`);
